18e731cb76b6b744450cd9cffda25fe72f0bf704
Jerin Johny <jerinjohny156@gmail.com>
2022-01-20 21:37:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -617,7 +617,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 
 async def auto_filter(client, msg, spoll=False):
-jjejissii
     if not spoll:
         message = msg
         settings = await get_settings(message.chat.id)
